75656ec65a6dd8a296834d365e9982f6054780ec
avedineev <avedineev69@gmail.com>
2023-08-26 18:30:19 +0300

    20.2 Шаблонизация с пагинацией

--- shop/views.py
+++ shop/views.py
@@ -1,14 +1,23 @@
 import json
 
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
-from shop.models import Flowers, Category
+from shop.models import Flowers
 
 
 def home(request):
+    bouquets_list = Flowers.objects.all()
+    # Постраничная разбивка с 3 букетами на страницу
+    paginator = Paginator(bouquets_list, 3)
+    page_numer = request.GET.get('page', 1)
+    bouquets_obj = paginator.get_page(page_numer)
+    print(bouquets_obj.object_list)
+
     context = {
-        'object_list': Flowers.objects.all()[:8],
-        'title': '<Букеты - интернет магазин>'
+        'object_list': bouquets_obj.object_list,
+        'title': '<Букеты - интернет магазин>',
+        'bouquets': bouquets_obj,
     }
     return render(request, 'shop/home.html', context)
 
@@ -37,10 +46,12 @@ def detail_info(request, pk):
     item = Flowers.objects.get(pk=pk)
     cat = item.category
     pr = item.price
+    img = item.Imag
     context = {
         'title': f'Букет -  {item.name}',
         'category': cat,
-        'price' : pr
+        'price': pr,
+        'img': img
     }
     print(context)
     return render(request, 'shop/detail_info.html', context)
