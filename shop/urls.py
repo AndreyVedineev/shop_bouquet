7f26126e2497e86caef00b3a8d51171afb18bb46
avedineev <avedineev69@gmail.com>
2023-09-04 18:21:27 +0300

    исправления по 22.2

--- shop/urls.py
+++ shop/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 
 from shop.apps import ShopConfig
 from shop.views import contacts, FlowersListView, FlowersDetailView, BlogCreateView, BlogListView, BlogDetailView, \
-    BlogUpdateView, BlogDeleteView, toggle_activity, FlowersCreateView, FlowersUpdateView, FlowersDeleteView
+    BlogUpdateView, BlogDeleteView, toggle_activity, FlowersCreateView, FlowersUpdateView, FlowersDeleteView, \
+    toggle_activity_fl
 
 app_name = ShopConfig.name
 
@@ -15,6 +16,7 @@ urlpatterns = [
     path("flowers/<int:pk>/detail/", FlowersDetailView.as_view(), name='flowers_detail/'),
     path("flowers/<int:pk>/update/", FlowersUpdateView.as_view(), name='flowers_update/'),
     path("flowers/<int:pk>/delete/", FlowersDeleteView.as_view(), name='flowers_delete/'),
+    path("flowers/<int:pk>/activiti_fl/", toggle_activity_fl, name='toggle_activity_fl/'),
 
     path("blog/create", BlogCreateView.as_view(), name='blog_create/'),
     path("blog/list/", BlogListView.as_view(), name='blog_list/'),
@@ -22,6 +24,8 @@ urlpatterns = [
     path("blog/<int:pk>/update/", BlogUpdateView.as_view(), name='blog_update/'),
     path("blog/<int:pk>/delete/", BlogDeleteView.as_view(), name='blog_delete/'),
     path("blog/<int:pk>/activiti/", toggle_activity, name='toggle_activity/'),
+
+
 
     path("contacts/", contacts, name='contacts.html'),
 
